81d7e4bc93c6c4ec412b0f5bea173475efadda27
OKAN AKSU <okanaksu_58@hotmail.com>
2024-06-08 16:53:08 +0300

    Proje dosyası ekle.

---
+++ CQRSNight/DAL/Context/CQRSContext.cs
@@ -0,0 +1,15 @@
+﻿using CQRSNight.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CQRSNight.DAL.Context
+{
+    public class CQRSContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-9B3JVRI;initial Catalog=CQRSDb;integrated Security=true");
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+    }
+}
